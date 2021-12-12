1b260eac34886cc3fe18f3503faaec7a395c8cf1
minapnh0 <64715590+minapnh0@users.noreply.github.com>
2021-12-12 06:26:36 -0500

    sign in

--- app/src/main/java/com/example/navdrawer/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/navdrawer/ui/home/HomeFragment.java
@@ -12,7 +12,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.navdrawer.R;
 import com.example.navdrawer.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
